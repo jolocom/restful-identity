ee048de6dbaa57e56bdb1a657f3a0e390f4a6530
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-01 22:35:12 +0200

    moved to typescript and added some dependancies

---
+++ app.ts
@@ -0,0 +1,16 @@
+import * as fastify from 'fastify';
+import * as fp from 'fastify-plugin';
+import { Server, IncomingMessage, ServerResponse } from "http";
+
+// route/app plugin
+import { identityService } from './services/identityService';
+
+const server: fastify.FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify({logger:true});
+
+server.register( identityService )
+
+const start 
