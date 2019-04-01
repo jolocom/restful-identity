f2d0d2823af7d4a0fed39f6636ef8fb30b5d2d5a
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-01 22:39:28 +0200

    add type for instance in identity

--- plugins/identity.ts
+++ plugins/identity.ts
@@ -1,8 +1,9 @@
+import * as fastify from 'fastify';
 import * as fp from 'fastify-plugin';
 
 import { JolocomLib } from 'jolocom-lib';
 
-export default fp(async (fastify, opts: {seed: Buffer, password: string }, next) => {
+export default fp(async (instance: fastify.FastifyInstance, opts: {seed: Buffer, password: string }, next) => {
   const vaultedKeyProvider = new JolocomLib.KeyProvider(opts.seed, opts.password);
   const registry = JolocomLib.registries.jolocom.create();
 
@@ -10,10 +11,10 @@ export default fp(async (fastify, opts: {seed: Buffer, password: string }, next)
     derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey,
     encryptionPass: opts.password,
   }).then(identityWallet =>
-          fastify.decorate('identity', identityWallet)
+          instance.decorate('identity', identityWallet)
          )
     .catch(err =>
-           fastify.log.error({ actor: "identity" }, err)
+           instance.log.error({ actor: "identity" }, err)
           )
 
   next();
