96db7509b475d84967252dbd40523a500ec71a98
Charles Cunningham <charles.cunningham@outlook.com>
2019-04-04 11:28:51 +0200

    broke up the app plugin and server

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
-import * as fastify from 'fastify';
-import { Server, IncomingMessage, ServerResponse } from "http";
+
+import * as fp from 'fastify-plugin';
 
 // utility plugins
 import identity from './plugins/identity';
@@ -8,33 +8,11 @@ import interactions from './plugins/interactions';
 // route/app plugin
 import identityService from './services/identityService';
 
-const server: fastify.FastifyInstance<
-  Server,
-  IncomingMessage,
-  ServerResponse
-> = fastify({logger:true});
+export default fp(async (instance: any, opts, next) => {
+  instance.register( identity, {idArgs: {seed: new Buffer('a'.repeat(64), 'hex'),
+                                       password: 'secret'}});
+  instance.register( interactions, {loki: {file: 'interactions.json',
+                                         collections: ['interactions']}});
+  instance.register( identityService, {service: {callbackURL: 'http://localhost:3000'}})
 
-server.register( identity, {idArgs: {seed: new Buffer('a'.repeat(64), 'hex'),
-                                     password: 'secret'}});
-server.register( interactions, {loki: {file: 'interactions.json',
-                                       collections: ['interactions']}});
-server.register( identityService, {service: {callbackURL: 'http://localhost:3000'}})
-
-const start = async () => {
-  try {
-    await server.listen(3000, "0.0.0.0");
-  } catch (err) {
-    console.log(err);
-    server.log.error(err);
-    process.exit(1);
-  }
-};
-
-process.on("uncaughtException", error => {
-  console.error(error);
-});
-process.on("unhandledRejection", error => {
-  console.error(error);
-});
-
-start();
+})
---
+++ server.ts
@@ -0,0 +1,32 @@
+
+import * as fastify from 'fastify';
+import { Server, IncomingMessage, ServerResponse } from "http";
+
+import app from './app';
+
+const server: fastify.FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify({logger:true});
+
+server.register( app, {service: {callbackURL: 'http://localhost:3000'}})
+
+const start = async () => {
+  try {
+    await server.listen(3000, "0.0.0.0");
+  } catch (err) {
+    console.log(err);
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+process.on("uncaughtException", error => {
+  console.error(error);
+});
+process.on("unhandledRejection", error => {
+  console.error(error);
+});
+
+start();
