a52ba9bfe202d9dc87320de25af694085c81407c
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-08-02 13:52:37 +0200

    adding schema for input validation

--- src/services/identityService.ts
+++ src/services/identityService.ts
@@ -1,30 +1,119 @@
 import * as fp from 'fastify-plugin';
 import identityController from '../plugins/identityController';
+import { IDParameters, ControllerInstance } from '../plugins/types'
 
-export default fp(async (instance: any, opts: {callbackURL: string, idArgs: {seed: any, password: string}}, next) =>
-                  {
-                    instance.register(identityController, opts);
+const authReqSchema = {
+    additionalProperties: false,
+    properties: {
+        callbackURL: {
+            type: "string"
+        },
+        description: {
+            type: "string"
+        }
+    },
+    required: [
+        "callbackURL",
+        "description"
+    ],
+    type: "object"
+}
 
-                    instance.get('/info', {},
-                                 (request, reply) => reply.send(instance.idC.getInfo()));
+const authRespSchema = {
+    additionalProperties: false,
+    properties: {
+        callbackURL: {
+            type: "string"
+        },
+        description: {
+            type: "string"
+        }
+    },
+    required: [
+        "callbackURL",
+        "description"
+    ],
+    type: "object"
+}
 
-                    instance.get('/authenticationRequest', {},
-                                 async (request, reply) => await instance.idC.getAuthenticationRequest()
-                                 .then(authReq => reply.send(authReq))
-                                 .catch(error => {request.log.error(error);
-                                                  reply.code(500)}));
+const paymentReqSchema = {
+    additionalProperties: false,
+    properties: {
+        callbackURL: {
+            type: "string"
+        },
+        description: {
+            type: "string"
+        },
+        transactionOptions: {
+            type: "object",
+            properties: {
+                value: {
+                    type: "number"
+                },
+                to: {
+                    type: "string"
+                },
+                gasLimit: {
+                    type: "number"
+                },
+                gasPrice: {
+                    type: "number"
+                },
+            },
+            required: ["value", "to", "gaslimit", "gasPrice"]
+        }
+    },
+    required: [
+        "callbackURL",
+        "description",
+        "transactionOptions"
+    ],
+    type: "object"
+}
 
-                    instance.get('/paymentRequest', {},
-                                 async (request, reply) => await instance.idC.getPaymentRequest()
-                                 .then(paymentReq => reply.send(paymentReq))
-                                 .catch(error => {request.log.error(error);
-                                                  reply.code(500)}));
+const validateSchema = {
+    additionalProperties: false,
+    properties: {
+        token: {
+            type: "string"
+        }
+    },
+    required: [
+        "token",
+    ],
+    type: "object"
+}
 
-                    instance.post('/validateResponse', {},
-                                  async (request, reply) => await instance.idC.isInteractionResponseValid(request.body.token)
-                                  .then(valid => {valid ? reply.code(204) : reply.code(409).send('Validation Failed')})
-                                  .catch(error => {request.log.error(error);
-                                                   reply.code(500)}));
+export default fp(async (instance: ControllerInstance, opts: { idArgs: IDParameters }, next) => {
+    instance.register(identityController, opts);
 
-                    next();
-                  });
+    instance.get('/did', {},
+        (request, reply) => reply.send(instance.idController.getInfo()));
+
+    instance.get('/authentication', {},
+        async (request, reply) => instance.idController.request.auth()
+            .then(authReq => reply.send(authReq))
+            .catch(error => {
+                request.log.error(error);
+                reply.code(500)
+            }));
+
+    instance.get('/payment', {},
+        async (request, reply) => await instance.idController.request.payment()
+            .then(paymentReq => reply.send(paymentReq))
+            .catch(error => {
+                request.log.error(error);
+                reply.code(500)
+            }));
+
+    instance.post('/validate', {},
+        async (request, reply) => await instance.idC.isInteractionResponseValid(request.body.token)
+            .then(valid => { valid ? reply.code(204) : reply.code(409).send('Validation Failed') })
+            .catch(error => {
+                request.log.error(error);
+                reply.code(500)
+            }));
+
+    next();
+});
