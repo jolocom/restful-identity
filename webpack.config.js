1d46d26d127120b1828e05734c2ae6d2ef412d54
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-08-27 10:12:44 +0200

    added shim stuff, compiles, doesnt work

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 
 const path = require('path');
 
+const pr = res => dir => file => res.resolve(dir, file);
+const prh = pr(path)(__dirname);
+const sh = prh('shim.js');
+
 module.exports = {
   entry: './src/server.ts',
   module: {
@@ -18,13 +22,27 @@ module.exports = {
   },
   target: 'node',
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: [ '.tsx', '.ts', '.js' ],
+    alias: {
+      electron: sh,
+      "./build/Release/scrypt": sh,
+      "uglify-es/package.json": sh,
+      "uglify-es": sh,
+      "rdf-canonize-native": sh,
+      "pino-pretty": sh,
+      "long": sh,
+      "../build/default/validation": sh,
+      "../build/default/bufferutil": sh,
+      "../build/Release/validation": sh,
+      "../build/Release/bufferutil": sh,
+      "./package": sh
+    }
   },
-  externals: {
-    
-  },
+    externals: {
+      
+    },
   output: {
     filename: 'server.js',
-    path: path.resolve(__dirname, 'dist')
+    path: prh('dist')
   }
 };
