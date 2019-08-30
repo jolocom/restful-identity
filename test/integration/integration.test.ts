5cf1efe37756f8a1538f94a7f58465051ff61ebf
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-08-30 10:13:53 +0200

    added keycloak to testing

--- test/integration/integration.test.ts
+++ test/integration/integration.test.ts
@@ -5,7 +5,10 @@ import * as supertest from 'supertest'
 import { IdentityWallet } from 'jolocom-lib/js/identityWallet/identityWallet';
 import { Server, IncomingMessage, ServerResponse } from "http";
 import { Authentication } from 'jolocom-lib/js/interactionTokens/authentication';
-import identity from '../../src/plugins/identity';
+import { CredentialShareRequestCreationArgs } from 'jolocom-lib/js/identityWallet/types';
+import { CredentialResponse } from 'jolocom-lib/js/interactionTokens/credentialResponse';
+import { JSONWebToken } from 'jolocom-lib/js/interactionTokens/JSONWebToken';
+import { CredentialRequest } from 'jolocom-lib/js/interactionTokens/credentialRequest';
 
 const seed = new Buffer('b'.repeat(64), 'hex');
 const pword = 'henlmao';
@@ -13,7 +16,8 @@ const vkp = JolocomLib.KeyProvider.fromSeed(seed, pword);
 
 const reg = JolocomLib.registries.jolocom.create();
 
-const url = 'http://localhost:3000';
+// const url = 'raspberrypi.local:3000';
+const url = 'http://localhost:3000'
 
 const tester = supertest(url)
 
@@ -28,7 +32,24 @@ fastify_instance.register(identityService);
 const basicAttrs = {callbackURL: 'https://google.com',
                     description: 'test description'}
 
+const credReqAttrs: CredentialShareRequestCreationArgs = {
+    callbackURL: 'https://google.com',
+    credentialRequirements: [
+        {
+            type: ['name'],
+            constraints: [{}]
+        },
+        {
+            type: ['email'],
+            constraints: [{}]
+        }
+    ]
+}
+
 let idw: IdentityWallet;
+let kcToken: JSONWebToken<CredentialRequest>;
+let kcBody;
+let authReqBody;
 
 const load = (testr: supertest.SuperTest<supertest.Test>) => endpoint => async (body) =>
     testr.post(endpoint)
@@ -39,13 +60,21 @@ const load = (testr: supertest.SuperTest<supertest.Test>) => endpoint => async (
 
 const authReq = load(tester)('/request/authentication')
 const authResp = load(tester)('/response/authentication')
+const keycloak = load(tester)('/response/keycloak')
 const validity = load(tester)('/validate')
 
-const doAuthFlow = (ident: IdentityWallet) => async (attrs) => {
-    const {body: {token: val}} = await authReq(attrs)
+const doAuthFlow = (ident: IdentityWallet) => async (authAttrs) => {
+    const {body: {token: val}} = await authReq(authAttrs)
     const token = JolocomLib.parse.interactionToken.fromJWT<Authentication>(val)
-    const response = await ident.create.interactionTokens.response.auth(attrs, pword, token)
-    return await validity({token: response.encode()}) }
+    const response = await ident.create.interactionTokens.response.auth(authAttrs, pword, token)
+    return validity({token: response.encode()}) }
+
+const doAllFlows = (ident: IdentityWallet) => async (authAttrs, authRespToken, kcToken) =>
+    Promise.all([
+        doAuthFlow(ident)(authAttrs),
+        keycloak(kcToken),
+        authResp(authRespToken)
+    ])
 
 describe('identity interaction integration test', () => {
   beforeAll(async () => {
@@ -53,17 +82,21 @@ describe('identity interaction integration test', () => {
 
     await fastify_instance.ready();
 
-    //await JolocomLib.util.fuelKeyWithEther(vkp.getPublicKey({
-    //  derivationPath: JolocomLib.KeyTypes.ethereumKey,
-    //  encryptionPass: pword
-    //}));
-
     idw = await reg.authenticate(vkp, {
       derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey,
       encryptionPass: pword
     });
 
+    kcToken = await idw.create.interactionTokens.request.share(credReqAttrs, pword)
+    kcBody = {request: kcToken.encode(),
+              attrs: {
+                name: "Scooter",
+                email: "scooter-email"
+              }}
 
+      authReqBody = {request: await idw.create.interactionTokens.request.auth(basicAttrs, pword).then(t => t.encode()),
+                     attrs: basicAttrs
+                  }
     fastify_instance.listen(3000, "0.0.0.0").then(console.log).catch(console.error)
   });
 
@@ -86,26 +119,19 @@ describe('identity interaction integration test', () => {
           .expect(201)
 
       const authReqP = JolocomLib.parse.interactionToken.fromJWT<Authentication>(authReq)
-      expect(authReq)
 
       const authResp = await idw.create.interactionTokens.response.auth(basicAttrs, pword, authReqP)
 
-      const val = tester.post('/validate')
-          .send({token: authResp.encode()})
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(201)
-          .then(val => expect(val.body.validity).toEqual(true))
+      const val = validity({token: authResp.encode()})
 
-      const otherReq = tester.post('/response/authentication')
-          .send({request: authReq,
-                 attrs: basicAttrs})
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(201)
+      const otherReq = keycloak(kcBody)
 
-      await otherReq
+      const kcr = await otherReq
       await val
+
+      const kcrP = JolocomLib.parse.interactionToken.fromJWT<CredentialResponse>(kcr.body.token)
+      expect(idw.validateJWT(kcrP, kcToken))
+      expect(kcrP.interactionToken.satisfiesRequest(kcToken.interactionToken))
 
       done()
   })
@@ -114,7 +140,7 @@ describe('identity interaction integration test', () => {
         const reqs = []
 
         for (let i = 0; i < 100; i++) {
-            reqs.push(doAuthFlow(idw)(basicAttrs))
+            reqs.push(doAllFlows(idw)(basicAttrs, authReqBody, kcBody))
         }
 
         await Promise.all(reqs)
